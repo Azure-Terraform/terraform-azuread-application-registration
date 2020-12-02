ec6420fb18b859705af622b902be4e61df8daeea
Tim Miller <tim.miller@risk.lexisnexis.com>
2020-12-01 19:35:30 -0500

    switch to newer terraform syntax

--- versions.tf
+++ versions.tf
@@ -1,7 +1,8 @@
 terraform {
-  required_version = ">= 0.12.10"
-}
-
-provider "azuread" {
-  version = ">= 0.7.0"
+  required_version = ">= 0.13.5"
+  required_providers {
+    azuread = {
+      version = ">= 0.7.0"
+    }
+  }
 }
